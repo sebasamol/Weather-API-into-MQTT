3852eaa9041dfa5b1421cebc1638d263d74636f9
sebasamol <sebasamol@gmail.com>
2025-03-24 21:12:47 +0100

    descrip

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 async function fetchData() {
     const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.4069&longitude=16.9299&current=temperature_2m")
     const data = await response.json()
-    console.log(data.current.temperature_2m)
+    console.log('Temperatura zewnętrzna:'+ data.current.temperature_2m)
 }
 
 fetchData()
