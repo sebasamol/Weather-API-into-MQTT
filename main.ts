ee85043319d745a68e77821f1b6d1981dfae45ba
sebasamol <sebasamol@gmail.com>
2025-03-24 20:39:50 +0100

    Test commit fetch Weather API

--- main.ts
+++ main.ts
@@ -1,2 +1,10 @@
+async function fetchData() {
+    const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.4069&longitude=16.9299&current=temperature_2m")
+    const data = await response.json()
+    console.log(data)
+}
+
+fetchData()
+
 console.log("Hello from GitHub")
 console.log("Looking for changes")
